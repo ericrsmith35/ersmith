8548d3a34ec4dcc9a09d0c1890edbe4cdeb1320a
Eric Smith <ericrsmith35@gmail.com>
2020-01-13 17:29:01 -0500

    batalien

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,92 @@
+declare module "@salesforce/resourceUrl/Action_Update_60" {
+    var Action_Update_60: string;
+    export default Action_Update_60;
+}
+declare module "@salesforce/resourceUrl/CloudQuestion" {
+    var CloudQuestion: string;
+    export default CloudQuestion;
+}
+declare module "@salesforce/resourceUrl/Conga_Display" {
+    var Conga_Display: string;
+    export default Conga_Display;
+}
+declare module "@salesforce/resourceUrl/Conga_Hover" {
+    var Conga_Hover: string;
+    export default Conga_Hover;
+}
+declare module "@salesforce/resourceUrl/Conga_Press" {
+    var Conga_Press: string;
+    export default Conga_Press;
+}
+declare module "@salesforce/resourceUrl/CopyOppStyle" {
+    var CopyOppStyle: string;
+    export default CopyOppStyle;
+}
+declare module "@salesforce/resourceUrl/Create_Ticket" {
+    var Create_Ticket: string;
+    export default Create_Ticket;
+}
+declare module "@salesforce/resourceUrl/FastOrderJs" {
+    var FastOrderJs: string;
+    export default FastOrderJs;
+}
+declare module "@salesforce/resourceUrl/Google_Map" {
+    var Google_Map: string;
+    export default Google_Map;
+}
+declare module "@salesforce/resourceUrl/GraphicsPack" {
+    var GraphicsPack: string;
+    export default GraphicsPack;
+}
+declare module "@salesforce/resourceUrl/GraphicsPackExample" {
+    var GraphicsPackExample: string;
+    export default GraphicsPackExample;
+}
+declare module "@salesforce/resourceUrl/GraphicsPackNew" {
+    var GraphicsPackNew: string;
+    export default GraphicsPackNew;
+}
+declare module "@salesforce/resourceUrl/Ideas_Image" {
+    var Ideas_Image: string;
+    export default Ideas_Image;
+}
+declare module "@salesforce/resourceUrl/LightningGraphicsPackExample" {
+    var LightningGraphicsPackExample: string;
+    export default LightningGraphicsPackExample;
+}
+declare module "@salesforce/resourceUrl/Opportunity_Logo" {
+    var Opportunity_Logo: string;
+    export default Opportunity_Logo;
+}
+declare module "@salesforce/resourceUrl/Opportunity_Logo_Orange" {
+    var Opportunity_Logo_Orange: string;
+    export default Opportunity_Logo_Orange;
+}
+declare module "@salesforce/resourceUrl/SLDS" {
+    var SLDS: string;
+    export default SLDS;
+}
+declare module "@salesforce/resourceUrl/SLDS0121_Order" {
+    var SLDS0121_Order: string;
+    export default SLDS0121_Order;
+}
+declare module "@salesforce/resourceUrl/SLDS202forLtng" {
+    var SLDS202forLtng: string;
+    export default SLDS202forLtng;
+}
+declare module "@salesforce/resourceUrl/SiteSamples" {
+    var SiteSamples: string;
+    export default SiteSamples;
+}
+declare module "@salesforce/resourceUrl/highlightjs" {
+    var highlightjs: string;
+    export default highlightjs;
+}
+declare module "@salesforce/resourceUrl/jQuery_LookupCmp" {
+    var jQuery_LookupCmp: string;
+    export default jQuery_LookupCmp;
+}
+declare module "@salesforce/resourceUrl/jqueryforltng" {
+    var jqueryforltng: string;
+    export default jqueryforltng;
+}
