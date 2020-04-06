104315d2acab994441a064da1893a13d8fde7020
ericrsmith35 <ericrsmith35@gmail.com>
2020-04-06 09:48:22 -0400

    bring todate

---
+++ .sfdx/typings/lwc/apex/LightningLookupController3.d.ts
@@ -0,0 +1,12 @@
+declare module "@salesforce/apex/LightningLookupController3.getRecords" {
+  export default function getRecords(param: {sObjectName: any, valueFieldName: any, displayedFieldName: any, otherFields: any, whereClause: any, filteredFieldName: any, filterFieldValue: any}): Promise<any>;
+}
+declare module "@salesforce/apex/LightningLookupController3.getHelpText" {
+  export default function getHelpText(param: {field: any}): Promise<any>;
+}
+declare module "@salesforce/apex/LightningLookupController3.getReference" {
+  export default function getReference(param: {field: any}): Promise<any>;
+}
+declare module "@salesforce/apex/LightningLookupController3.getFieldValue" {
+  export default function getFieldValue(param: {obj: any, objId: any, label: any, whereField: any}): Promise<any>;
+}
